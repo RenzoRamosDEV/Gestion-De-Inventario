ecbabe3e3c352d32a971819541395792ca675045
Renzo Ramos <renzoramosivan@gmail.com>
2026-04-30 19:32:06 +0200

    feat: excepción personalizada para validaciones de negocio

---
+++ entity-service/src/main/java/org/service_entity/entity/exception/BusinessValidationException.java
@@ -0,0 +1,15 @@
+package org.service_entity.entity.exception;
+
+/**
+ * Excepción para validaciones de negocio.
+ */
+public class BusinessValidationException extends RuntimeException {
+    
+    public BusinessValidationException(String message) {
+        super(message);
+    }
+    
+    public BusinessValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
