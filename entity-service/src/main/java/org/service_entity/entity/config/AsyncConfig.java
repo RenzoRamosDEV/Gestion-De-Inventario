2bc5a70a31c44c7a0f19871c172f3496fbfbbbb2
Renzo Ramos <renzoramosivan@gmail.com>
2026-04-30 19:33:13 +0200

    feat: configuración para operaciones asíncronas

---
+++ entity-service/src/main/java/org/service_entity/entity/config/AsyncConfig.java
@@ -0,0 +1,26 @@
+package org.service_entity.entity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * Configuración para operaciones asíncronas.
+ */
+@Configuration
+@Profile("!test")
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("inventario-");
+        executor.initialize();
+        return executor;
+    }
+}
