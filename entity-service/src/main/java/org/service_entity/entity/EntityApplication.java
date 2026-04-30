5645f5ec0d902da1b9ecd6486c57aed74eb0ff9f
Renzo Ramos <renzoramosivan@gmail.com>
2026-04-30 19:27:48 +0200

    docs: documentación Javadoc en clase principal de aplicación

--- entity-service/src/main/java/org/service_entity/entity/EntityApplication.java
+++ entity-service/src/main/java/org/service_entity/entity/EntityApplication.java
@@ -3,6 +3,12 @@ package org.service_entity.entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Aplicación principal del sistema de gestión de inventario.
+ * 
+ * @author Sistema de Inventario
+ * @version 1.0.0
+ */
 @SpringBootApplication
 public class EntityApplication {
 
